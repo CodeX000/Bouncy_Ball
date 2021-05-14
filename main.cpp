7b34761dbf0aa3b63d064d22fcd179c80a05830d
Corey Fortin <55935808+CodeX000@users.noreply.github.com>
2021-05-13 22:18:21 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
-#include "../galacticMap/galacticMap/star.h"
 #include <stdlib.h>
 
 class MapGenerator : public olc::PixelGameEngine
@@ -11,8 +10,6 @@ public:
 		sAppName = "Galactic Map";
 	}
 public:
-	//const static int NUMBER_OF_STARS = 25;
-
 	//Accel = speed/frame
 	double gravityAccel = 0.4;
 
@@ -27,17 +24,11 @@ public:
 public:
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
-		/*for (int i = 0; i < NUMBER_OF_STARS; i++) {
-			stars[i] = star::Star(rand() % ScreenWidth(), rand() % ScreenHeight(), rand() % 15 + 5);
-		}*/
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-
 		/* Horizontal Traverse */
 		if (GetKey(olc::Key::A).bHeld) {
 			if (ballXPos < 10.0)
@@ -74,18 +65,12 @@ public:
 		DrawLine(10, ScreenHeight() - 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::WHITE);
 		DrawLine(ScreenWidth() - 10, 10, ScreenWidth() - 10, ScreenHeight() - 10, olc::WHITE);
 
-		/*
-		for (int i = 0; i < NUMBER_OF_STARS; i++) {
-			FillCircle(stars[i].getX(), stars[i].getY(), stars[i].getSize(), olc::YELLOW);
-		}*/
-
 		FillCircle(ballXPos, ballYPos, ballSize, olc::WHITE);
 		return true;
 	}
-	// Optional!
+	
 	bool OnUserDestroy() override
 	{
-		// Called when window is closed
 		return true;
 	}
 };
